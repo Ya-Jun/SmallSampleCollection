43e163e029b12413c9bcddfb267c1e7398c11458
王亚军 <ChampioWang@163.com>
2017-12-22 12:12:57 +0800

    提交DragLayoutDemo

--- DragLayoutDemo/src/main/java/com/wyj/myapplication/GestureSwitchLayout.java
+++ DragLayoutDemo/src/main/java/com/wyj/myapplication/GestureSwitchLayout.java
@@ -15,6 +15,8 @@ public class GestureSwitchLayout extends FrameLayout {
     private ViewDragHelper dragHelper;
     private View mDragView, contentView_bottom, contentView_top;
     private OnViewDragStateChangedListener onViewDragStateChangedListener;
+    public static final int STATE_IDLE_TOP = 1;
+    public static final int STATE_IDLE_BOTTOM = 2;
 
     public GestureSwitchLayout(Context context) {
         super(context);
@@ -57,15 +59,18 @@ public class GestureSwitchLayout extends FrameLayout {
         public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
 //            contentView_top.layout(0, -(contentView_top.getHeight() - top), getWidth(), top);
 //            contentView_bottom.layout(0, top + mDragView.getHeight(), getWidth(), top + mDragView.getHeight() + contentView_bottom.getHeight());
-
             contentView_top.offsetTopAndBottom(dy);
             contentView_bottom.offsetTopAndBottom(dy);
 
-            System.out.println("---------------------------------移动中top" + contentView_top.getTop());
-            System.out.println("---------------------------------移动中" + mDragView.getTop());
-            System.out.println("---------------------------------移动中bottom" + contentView_bottom.getTop());
-            System.out.println("---------------------------------");
-
+            if (top == changedView.getHeight()) {//向下滑动停止
+                if (onViewDragStateChangedListener != null) {
+                    onViewDragStateChangedListener.OnViewDragStateChanged(STATE_IDLE_BOTTOM);
+                }
+            } else if (top == -changedView.getHeight()) {//向上滑动停止
+                if (onViewDragStateChangedListener != null) {
+                    onViewDragStateChangedListener.OnViewDragStateChanged(STATE_IDLE_TOP);
+                }
+            }
         }
 
         @Override
@@ -85,23 +90,12 @@ public class GestureSwitchLayout extends FrameLayout {
             if (state == ViewDragHelper.STATE_DRAGGING) {
 
             } else if (state == ViewDragHelper.STATE_IDLE) {
-                System.out.println("---------------------------------还原" + contentView_top.getTop());
-                System.out.println("---------------------------------还原" + mDragView.getTop());
-                System.out.println("---------------------------------还原" + contentView_bottom.getTop());
-
-                initLayout();
-
 //                contentView_top.offsetTopAndBottom(-mDragView.getTop());
 //                contentView_bottom.offsetTopAndBottom(-mDragView.getTop());
 //                mDragView.offsetTopAndBottom(-mDragView.getTop());
 
-                System.out.println("---------------------------------还原完成上" + contentView_top.getTop());
-                System.out.println("---------------------------------还原完成中" + mDragView.getTop());
-                System.out.println("---------------------------------还原完成下" + contentView_bottom.getTop());
-
-            }
-            if (onViewDragStateChangedListener != null) {
-                onViewDragStateChangedListener.OnViewDragStateChanged(state);
+                // 滑动结束，还原各view位置到初始化状态
+                initLayout();
             }
         }
 
@@ -120,16 +114,12 @@ public class GestureSwitchLayout extends FrameLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 //        super.onLayout(changed, l, t, r, b);
         if (changed) {
-            System.out.println("---------------------------------初始化");
             initLayout();
         } else {
             contentView_top.layout(0, contentView_top.getTop(), getWidth(), contentView_top.getTop() + getHeight());
             mDragView.layout(0, mDragView.getTop(), getWidth(), mDragView.getTop() + getHeight());
             contentView_bottom.layout(0, contentView_bottom.getTop(), getWidth(), contentView_bottom.getTop() + getHeight());
         }
-        System.out.println("---------------------------------onLayout上" + contentView_top.getTop());
-        System.out.println("---------------------------------onLayout中" + mDragView.getTop());
-        System.out.println("---------------------------------onLayout下" + contentView_bottom.getTop());
     }
 
     private void initLayout() {
@@ -150,21 +140,18 @@ public class GestureSwitchLayout extends FrameLayout {
     }
 
     private void smoothToRestore() {
-        System.out.println("---------------------------------smoothToRestore");
         if (dragHelper.smoothSlideViewTo(mDragView, getPaddingLeft(), 0)) {
             ViewCompat.postInvalidateOnAnimation(this);
         }
     }
 
     private void smoothToTop() {
-        System.out.println("---------------------------------smoothToTop");
         if (dragHelper.smoothSlideViewTo(mDragView, getPaddingLeft(), -getHeight())) {
             ViewCompat.postInvalidateOnAnimation(this);
         }
     }
 
     private void smoothToBottom() {
-        System.out.println("---------------------------------smoothToBottom");
         if (dragHelper.smoothSlideViewTo(mDragView, getPaddingLeft(), getHeight())) {
             ViewCompat.postInvalidateOnAnimation(this);
         }
--- DragLayoutDemo/src/main/java/com/wyj/myapplication/MainActivity.java
+++ DragLayoutDemo/src/main/java/com/wyj/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.wyj.myapplication;
 
 import android.os.Bundle;
-import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,19 +20,21 @@ public class MainActivity extends AppCompatActivity {
         ImageView view2 = (ImageView) findViewById(R.id.imageView3);
         iv_loading = (ImageView) findViewById(R.id.iv);
 
-        view1.setBackgroundResource(R.drawable.bg_audio_bg_one);
-        view2.setBackgroundResource(R.drawable.bg_audio_bg_two);
-        iv_loading.setBackgroundResource(R.drawable.bg_audio_bg_three);
+        view1.setBackgroundResource(R.drawable.bg_one);
+        view2.setBackgroundResource(R.drawable.bg_two);
+
 
         gestureSwitchLayout.setOnViewDragStateChangedListener(new GestureSwitchLayout.OnViewDragStateChangedListener() {
             @Override
             public void OnViewDragStateChanged(int state) {
-                if (state == ViewDragHelper.STATE_IDLE) {
-                    System.out.println("---------------------------------iv_loading.setVisibility(View.VISIBLE);");
-                    iv_loading.setVisibility(View.VISIBLE);
-                    iv_loading.removeCallbacks(mRun);
-                    iv_loading.postDelayed(mRun, 3000);
+                iv_loading.setVisibility(View.VISIBLE);
+                if (state == GestureSwitchLayout.STATE_IDLE_TOP) {
+                    iv_loading.setBackgroundResource(R.drawable.bg_one);
+                } else if (state == GestureSwitchLayout.STATE_IDLE_BOTTOM) {
+                    iv_loading.setBackgroundResource(R.drawable.bg_two);
                 }
+                iv_loading.removeCallbacks(mRun);
+                iv_loading.postDelayed(mRun, 3000);
             }
         });
 
@@ -41,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         mRun = new Runnable() {
             @Override
             public void run() {
-                System.out.println("---------------------------------   iv_loading.setVisibility(View.GONE);");
                 iv_loading.setVisibility(View.GONE);
             }
         };
